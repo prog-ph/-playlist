e3a8c68a3b4b778f6180e717f75495b1035da994
Paulo henrique <108626073+prog-ph@users.noreply.github.com>
2022-08-28 22:03:04 -0300

    Commit

---
+++ videos.js
@@ -0,0 +1,33 @@
+export default [
+  {
+    src: "./img/01.mp4",
+    title: "Aranha-ceus",
+    display: "flex",
+
+  },
+  {
+    src: "./img/02.mp4",
+    title: "floresta",
+  },
+  {
+    src: "./img/03.mp4",
+    title: "Gato",
+  },
+  {
+    src: "./img/04.mp4",
+    title: "Home office",
+  },
+  {
+    src: "./img/05.mp4",
+    title: "Likes",
+    
+  },
+  {
+    src: "./img/06.mp4",
+    title: "Gota de água",
+  },
+  {
+    src: "./img/07.mp4",
+    title: "Código",
+  },
+];
